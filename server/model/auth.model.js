82cf04c9a5038ba2f1e1f5a149bdf433399aef9b
Mustafa Çor <mmustafaa9@gmail.com>
2022-01-20 14:36:06 +0300

    Update auth.model.js

--- server/model/auth.model.js
+++ server/model/auth.model.js
@@ -1,8 +1,7 @@
-const connect = require('../config/db');
+const db = require('../config/db').promise();
 const userModel = require('./user.model');
 module.exports.login = async (username,password)=>{
-    let con = await connect();
-    let [rows] = await con.query("SELECT * FROM users WHERE username = ? AND password = ?",[username,password]);
+    let [rows] = await db.query("SELECT * FROM users WHERE username = ? AND password = ?",[username,password]);
     if(rows.length > 0){ 
         return true;
     }
