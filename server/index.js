50c772844c0f8fd524d01b8c1fb718c7252c325c
Mustafa Çor <mmustafaa9@gmail.com>
2022-01-23 18:11:24 +0300

    cors fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 app.use(express.json()); // for parsing application/json to req.body for object
-
+app.use(require("cors")())
 const userRoute = require('./routes/user.route');
 const authRoute = require('./routes/auth.route');
 
